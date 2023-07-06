3c7d5985511282a0d63fe72a8bbfaaaa6be820dd
Bhuvanesh T G <gbhuvanesh100@gmail.com>
2023-07-06 18:43:52 +0530

    Remove authorization for product view and index

--- app/Http/Controllers/v1/ProductController.php
+++ app/Http/Controllers/v1/ProductController.php
@@ -11,15 +11,11 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $this->authorize('viewAny',Product::class);
-
         return ProductResource::collection(Product::all());
     }
 
     public function show(Product $product)
     {
-        $this->authorize('view',$product);
-
         return new ProductResource($product);
     }
 }
